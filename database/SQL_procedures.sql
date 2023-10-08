65402d1c2fbef9f5f26a91a2b2897d438e99c866
Iruda Chirath <iruda.21@cse.mrt.ac.lk>
2023-10-08 17:21:23 +0530

    updated sql procedures

--- database/SQL_procedures.sql
+++ database/SQL_procedures.sql
@@ -29,3 +29,38 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_item_with_configuration`(
+	SKU VARCHAR(50), -- Product SKU to get the product id
+    Price DECIMAL(9,2), 
+    Quantity INT,
+    Image VARCHAR(255), -- image path
+    No_of_variants INT,
+    Variant_types VARCHAR(255), -- comma separated list of variant names
+    Attributes VARCHAR(255) -- comma separated list of attribute names
+)
+BEGIN
+	DECLARE NewItemID INT;
+    DECLARE Product_id INT;
+    DECLARE Curr_variant_id INT;
+    DECLARE Curr_attribute_id INT;
+
+	SELECT p.Product_id INTO Product_id FROM product p WHERE SKU = p.SKU;
+
+    -- Add the item to the item table
+    INSERT INTO item (Product_id, Price, Quantity, Image) VALUES (Product_id, Price, Quantity, Image);
+    SET NewItemID = LAST_INSERT_ID();
+    
+    WHILE No_of_variants > 0 DO
+		SELECT variant_id INTO Curr_variant_id FROM variant WHERE name = TRIM(SUBSTRING_INDEX(TRIM(Variant_types), ',', 1));  -- Extract the first variant_id
+        SELECT attribute_id INTO Curr_attribute_id FROM attribute WHERE name = TRIM(SUBSTRING_INDEX(TRIM(Attributes), ',', 1)) AND variant_id = Curr_variant_id;  -- Extract the first attribute_id
+        INSERT INTO item_configuration VALUES (NewItemID, Curr_attribute_id);
+        SET Variant_types = SUBSTRING(TRIM(Variant_types), LENGTH(SUBSTRING_INDEX(Variant_types, ',', 1)) + 2);  -- Remove the inserted Variant name and comma
+        SET Attributes = SUBSTRING(TRIM(Attributes), LENGTH(SUBSTRING_INDEX(Attributes, ',', 1)) + 2);  -- Remove the inserted Attribute name and comma
+		SET No_of_variants = No_of_variants - 1;
+    END WHILE;
+END$$
+
+DELIMITER ;
