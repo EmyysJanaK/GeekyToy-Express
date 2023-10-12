4321bbec4d12956721e6d3b7410b9d5625bc860a
Yapa-UOM <savindayapa.21@cse.mrt.ac.lk>
2023-10-12 12:57:03 +0530

    GetMinPrice function updated
    Updated the datatype of price of the function

--- database/Setup_Queries.sql
+++ database/Setup_Queries.sql
@@ -165,9 +165,9 @@ DELIMITER ;
 
 -- Get the minimum price of a product given the product_id
 CREATE FUNCTION GetMinPrice(product_id INT)
-RETURNS DECIMAL(10,2)
+RETURNS DECIMAL(9,2)
 BEGIN
-    DECLARE min_price DECIMAL(10,2);
+    DECLARE min_price DECIMAL(9,2);
     SELECT MIN(price) INTO min_price
     FROM item
     WHERE product_id = product_id;
