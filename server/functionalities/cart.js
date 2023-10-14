51cea44938c8b585ec81ba3f94074de587e33db9
harithmaduranga <nharithmaduranga@gmail.com>
2023-10-15 02:21:51 +0530

    add to cart

--- server/functionalities/cart.js
+++ server/functionalities/cart.js
@@ -21,23 +21,30 @@ router.get('/:id', async (req,res)=>{
     console.log(cart_item);
 });
 
-// Add a new item to the cart of a user
 router.post('/:id/:item', async (req,res)=>{
-    const sql1 = `select is_registered from customers where customer_id = ?`;
-    const sql2 = `select cart_id from carts where customer_id = ?`;
-    const sql3 = `insert into carts values(?,0)`;
-    const sql4 = `insert into cart_items(cart_id, unique_product_id, quantity)
-                        values(?,?,1)
-                    on duplicate key
-                        update quantity = quantity + 1 where cart_id = ?`;
-    let is_registered = await db.query(sql1,req.params.id);
-    if(!is_registered) res.send("You are not registered yet. Please get registered.");
-    else{
-        let cart_id = await db.query(sql2,req.params.id);
-        if(!cart_id) db.query(sql3,req.params.id);
-        cart_id = await db.query(sql2,req.params.id);
-        db.query(sql4,[cart_id,req.params.item,cart_id]);
-    }
-}); 
+    const sql = `insert into cart_items values(?,?,?)
+                    on duplicate key update 
+                    quantity = quantity+1`;
+    db.query(sql,[req.params.id,req.params.item,1]);
+});
+
+// Add a new item to the cart of a user
+// router.post('/:id/:item', async (req,res)=>{
+//     const sql1 = `select is_registered from customers where customer_id = ?`;
+//     const sql2 = `select cart_id from carts where customer_id = ?`;
+//     const sql3 = `insert into carts values(?,0)`;
+//     const sql4 = `insert into cart_items(cart_id, unique_product_id, quantity)
+//                         values(?,?,1)
+//                     on duplicate key
+//                         update quantity = quantity + 1 where cart_id = ?`;
+//     let is_registered = await db.query(sql1,req.params.id);
+//     if(!is_registered) res.send("You are not registered yet. Please get registered.");
+//     else{
+//         let cart_id = await db.query(sql2,req.params.id);
+//         if(!cart_id) db.query(sql3,req.params.id);
+//         cart_id = await db.query(sql2,req.params.id);
+//         db.query(sql4,[cart_id,req.params.item,cart_id]);
+//     }
+// }); 
 
 module.exports = router;
--- server/services/productServices.js
+++ server/services/productServices.js
@@ -6,8 +6,15 @@ module.exports.getAllProducts = async () => {
 }
 
 module.exports.getUniqueProduct = async (id) => {
-    const [unique_product] = await db.query("SELECT * FROM products WHERE Product_id = ?", [id]);
-    return unique_product;
+    const [unique_product] = await db.query(`
+        SELECT Product_id,Title,SKU,Weight,Description,p.Image,Main_category,
+                    MIN(price) as Min_price
+        FROM products p
+        JOIN unique_products up using(product_id)
+        WHERE Product_id = ?`, [id]);
+    const [items] = await db.query(`
+        SELECT * FROM unique_products WHERE Product_id = ?`, [id]);
+    return [unique_product,items];
 }
 
 module.exports.deleteUniqueProduct = async (id) => {
